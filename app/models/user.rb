eb741334ab2800bfb520f626becaca39627109e1
Lee Azzarello <lee@rockingtiger.com>
2018-05-15 17:11:18 -0700

    add api token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  before_validation :ensure_token
   has_secure_password
+
+  def ensure_token
+    self.token = generate_hex(:token) unless token.present?
+  end
+
+  def generate_hex(column)
+    loop do
+      hex = SecureRandom.hex
+      break hex unless self.class.where(column => hex).any?
+    end
+  end
 end
