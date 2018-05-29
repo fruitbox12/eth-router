038723279c03808354496db9878e27e0d80d1a40
Lee Azzarello <lee@rockingtiger.com>
2018-05-29 14:58:28 -0700

    handle both http and websocket connections

--- proxy.js
+++ proxy.js
@@ -4,14 +4,17 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxyServer({});
-httpProxy.createServer({
-  target: 'ws://localhost:8546',
-  ws: true
-}).listen(9001);
+var proxy = httpProxy.createProxyServer({ws: true});
 
-http.createServer(function(req,res) {
+proxyServer = http.createServer(function(req,res) {
   proxy.web(req,res,{ target: 'http://localhost:8545'});
-}).listen(9000);
+});
 
-console.log("listening on port 9000 and 9001");
+// upgradable proxy.
+// uses the WebSocket upgrade event to switch to a ws proxy
+proxyServer.on('upgrade', function(req,socket,head) {
+  proxy.ws(req,socket,head, {target: 'ws://localhost:8546'});
+});
+
+proxyServer.listen(9000);
+console.log("listening on port 9000");
