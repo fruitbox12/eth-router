3dc30b91114581f1690942f435e515a368fdf640
Lee Azzarello <lee@rockingtiger.com>
2018-05-29 14:23:23 -0700

    add websocket support

--- proxy.js
+++ proxy.js
@@ -1,13 +1,17 @@
 // A reverse proxy server for many ethereum blockchain backends
-// Requires a valid web3 HTTP or WebSocket endpoint
+// Requires a valid web3 HTTP and WebSocket endpoint
 
 var http = require('http'),
     httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxyServer({});
+httpProxy.createServer({
+  target: 'ws://localhost:8546',
+  ws: true
+}).listen(9001);
 
-var server = http.createServer(function(req,res) {
+http.createServer(function(req,res) {
   proxy.web(req,res,{ target: 'http://localhost:8545'});
-});
-console.log("listening on port 9000");
-server.listen(9000);
+}).listen(9000);
+
+console.log("listening on port 9000 and 9001");
