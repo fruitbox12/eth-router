555b6af8097e66dd29cc935553daca811a71a498
Lee Azzarello <lee@rockingtiger.com>
2018-05-29 15:26:21 -0700

    use a hard coded API key for access

--- proxy.js
+++ proxy.js
@@ -2,18 +2,34 @@
 // Requires a valid web3 HTTP and WebSocket endpoint
 
 var http = require('http'),
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    url = require('url');
+
+const querystring = require('querystring');
 
 var proxy = httpProxy.createProxyServer({ws: true});
+var tokenRecord = 'foo';
 
 proxyServer = http.createServer(function(req,res) {
-  proxy.web(req,res,{ target: 'http://localhost:8545'});
+  var query = url.parse(req.url, true).query;
+  if ( query['token'] === tokenRecord) {
+    proxy.web(req,res,{ target: 'http://localhost:8545'});
+  } else {
+    res.write('access denied');
+    res.end();
+  }
 });
 
 // upgradable proxy.
 // uses the WebSocket upgrade event to switch to a ws proxy
 proxyServer.on('upgrade', function(req,socket,head) {
-  proxy.ws(req,socket,head, {target: 'ws://localhost:8546'});
+  var query = url.parse(req.url, true).query;
+  if ( query['token'] === tokenRecord) {
+    proxy.ws(req,socket,head, {target: 'ws://localhost:8546'});
+  } else {
+    socket.write('access denied');
+    socket.end();
+  }
 });
 
 proxyServer.listen(9000);
