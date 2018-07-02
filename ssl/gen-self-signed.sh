f33a613f534549013b7bf620f60adfdd06487542
aguestuser <aguestuser@riseup.net>
2018-07-02 16:11:54 -0400

    [ag] make `gen-self-signed` safely runnable from any directory
    * accomplish this by storing current dir, then cd'ing to script dir,
      then returning to original current dir after script executes

--- ssl/gen-self-signed.sh
+++ ssl/gen-self-signed.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+pushd `pwd` # save original path
+cd `dirname "$0"` # cd to script path
+
 # generate a self-signed SSL certificate
 # requires openssl
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+
+popd # return to original path
