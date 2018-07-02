76acca216bf551aace5a7dc4b8f7ba3f539017b7
Lee Azzarello <lee@rockingtiger.com>
2018-07-02 11:53:58 -0700

    remove Rails project

--- app/controllers/parity_controller.rb
+++
@@ -1,9 +0,0 @@
-class ParityController < ApplicationController
-  def kovan
-    reverse_proxy "http://localhost:8548", verify_ssl: false
-  end
-
-  def mainnet
-    reverse_proxy "http://localhost:8546", verify_ssl: false
-  end
-end
--- test/controllers/geth_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GethControllerTest < ActionDispatch::IntegrationTest
-  test "should get ropsten" do
-    get geth_ropsten_url
-    assert_response :success
-  end
-
-end
