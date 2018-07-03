414cb116cae8449bcfb961e2b8640a9faa695a73
aguestuser <aguestuser@riseup.net>
2018-07-03 17:26:17 -0400

    refactor secureProxy a bit (to prepare for test harness)

--- src/secureProxy.js
+++ src/secureProxy.js
@@ -1,41 +1,58 @@
 // A reverse proxy server for many ethereum blockchain backends
 // Requires a valid web3 HTTP and WebSocket endpoint
 
-var http = require('http'),
-    httpProxy = require('http-proxy'),
-    url = require('url'),
-    fs = require('fs');
+const http = require('http'),
+      httpProxy = require('http-proxy'),
+      url = require('url'),
+      fs = require('fs'),
+      querystring = require('querystring'),
+      tokenRecord = 'foo',
+      targetHttpPort = 8545,
+      targetWsPort = 8546,
+      proxyPort = 3000;
 
-const querystring = require('querystring');
-var tokenRecord = 'foo';
+const run = () => {
+  const server = handleProxyRequests(initialize())
+  server.listen(proxyPort);
+  console.log(`Listening on HTTPS port ${proxyPort}`);
+}
 
-// web3 library support for SSL is not supported for self-signed certificates
-// cURL is. See README for details.
-secureProxyServer = httpProxy.createServer(
-{ target: 'http://localhost:8545',
-  ssl: {
-    key: fs.readFileSync('ssl/key.pem', 'utf8'),
-    cert: fs.readFileSync('ssl/cert.pem', 'utf8')
-  }
-});
+const initialize = () => {
+  // web3 support for SSL not supported for self-signed certs; cURL is. See README for details.
+  return httpProxy.createServer({
+    target: `http://localhost:${targetHttpPort}`,
+    ssl: {
+      key: fs.readFileSync('ssl/key.pem', 'utf8'),
+      cert: fs.readFileSync('ssl/cert.pem', 'utf8')
+    }
+  })
+}
 
-// this is an event called proxyReq. It is called after the TLS handshake and before passing the request to the backend. This is where token authentication should happen
-secureProxyServer.on('proxyReq', function(proxyReq, req, res, options) {
-  var query = url.parse(req.url, true).query;
-  console.log("Got HTTPS request with token " + query['token'])
-  if ( query['token'] !== tokenRecord) {
-    res.write('access denied');
-    res.end();
-  }
-  secureProxyServer.on('upgrade', function(req,socket,head) {
-    var proxy = httpProxy.createProxyServer({
-      ws: true
-    });
-    //var query = url.parse(req.url, true).query;
-    console.log("Got WSS upgrade connection");
-    proxy.ws(req,socket,head, {target: 'ws://localhost:8546'});
+const handleProxyRequests = server => {
+  // 'proxyReq' event is called after TLS handshake and before passing the request to the backend.
+  server.on('proxyReq', function(proxyReq, req, res, options) {
+    authenticate(req, res)
+    // curious: does ordering matter here?
+    handleWsUpgrade(server)
   });
-});
+  return server
+}
 
-secureProxyServer.listen(3000);
-console.log("listening on HTTPS port 3000");
+const authenticate = (req, res)  => {
+  const query = url.parse(req.url, true).query
+  console.log(`Received request with token: ${query['token']}`)
+  if (query['token'] !== tokenRecord) {
+    res.write('access denied')
+    res.end()
+  }
+}
+
+const handleWsUpgrade = server => {
+  server.on('upgrade', (req, socket, head) => {
+    const proxy = httpProxy.createProxyServer({ ws: true })
+    console.log("Got WSS upgrade connection");
+    proxy.ws(req, socket, head, { target: `ws://localhost:${targetWsPort}` })
+  })
+}
+
+module.exports = { run }
