5e62798afdf3cc3230cf698f1557ae023656b95d
aguestuser <aguestuser@riseup.net>
2018-07-04 19:58:33 -0400

    [ag] add test harness (temporarily disable tls)
    * test that proxying works
    * test that (dummy) authentication works
    * disable tls for now, until local CA fix can enable tests to pass
    * todo: test websocket connections

--- src/secureProxy.js
+++ src/secureProxy.js
@@ -1,11 +1,9 @@
 // A reverse proxy server for many ethereum blockchain backends
 // Requires a valid web3 HTTP and WebSocket endpoint
 
-const http = require('http'),
-      httpProxy = require('http-proxy'),
+const httpProxy = require('http-proxy'),
       url = require('url'),
       fs = require('fs'),
-      querystring = require('querystring'),
       tokenRecord = 'foo',
       targetHttpPort = 8545,
       targetWsPort = 8546,
@@ -15,16 +13,22 @@ const run = () => {
   const server = handleProxyRequests(initialize())
   server.listen(proxyPort);
   console.log(`Listening on HTTPS port ${proxyPort}`);
+  return server
 }
 
 const initialize = () => {
   // web3 support for SSL not supported for self-signed certs; cURL is. See README for details.
   return httpProxy.createServer({
     target: `http://localhost:${targetHttpPort}`,
-    ssl: {
-      key: fs.readFileSync('ssl/key.pem', 'utf8'),
-      cert: fs.readFileSync('ssl/cert.pem', 'utf8')
-    }
+
+    // TODO: (aguestuser|04 Jul 2018)
+    // restore this once local CA enables passing tests
+    // see: https://stackoverflow.com/questions/19665863/how-do-i-use-a-self-signed-certificate-for-a-https-node-js-server/24749608#24749608
+
+    // ssl: {
+    //   key: fs.readFileSync('ssl/key.pem', 'utf8'),
+    //   cert: fs.readFileSync('ssl/cert.pem', 'utf8')
+    // }
   })
 }
 
@@ -42,17 +46,26 @@ const authenticate = (req, res)  => {
   const query = url.parse(req.url, true).query
   console.log(`Received request with token: ${query['token']}`)
   if (query['token'] !== tokenRecord) {
-    res.write('access denied')
+    res.writeHead(401, {'Content-Type': 'application/json' })
+    res.write(JSON.stringify({ error: "access denied" }))
     res.end()
   }
 }
 
 const handleWsUpgrade = server => {
   server.on('upgrade', (req, socket, head) => {
-    const proxy = httpProxy.createProxyServer({ ws: true })
     console.log("Got WSS upgrade connection");
-    proxy.ws(req, socket, head, { target: `ws://localhost:${targetWsPort}` })
+    httpProxy
+      .createProxyServer({ ws: true })
+      .ws(req, socket, head, {
+        target: `ws://localhost:${targetWsPort}`
+      })
   })
 }
 
-module.exports = { run }
+module.exports = {
+  run,
+  targetHttpPort,
+  targetWsPort,
+  proxyPort,
+}
