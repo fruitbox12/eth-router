642f52006fb396a7e40c0d96b6f96d58f46a87e5
aguestuser <aguestuser@riseup.net>
2018-07-18 13:54:50 -0400

    [ag|BIG CHANGE] remove TLS-handling from node proxy, add WS auth
    * instead of doing SSL termination in node proxy, handle this in nginx layer
    * check for tokens when WS upgrade requests are received, close
      connection if not present

--- src/proxy.js
+++ src/proxy.js
@@ -1,132 +1,52 @@
-const https = require("https")
+const httpProxy = require('http-proxy')
 const http = require("http")
 const {tokens} = require("./config")
+const url = require('url')
+const targetHost = 'localhost'
+const targetHttpPort = 8545
+const targetWsPort = 8546
+const proxyPort = 3000
 
-// A reverse proxy server for many ethereum blockchain backends
-// Requires a valid web3 HTTP and WebSocket endpoint on target server
+// HERE LIES: A reverse proxy server for many ethereum blockchain backends
+// - Requires valid web3 HTTP and WebSocket endpoints on target server
+// - Proxy listens on port 3000, assumes
+// - Proxies http requests to port 8545 of target, ws connections to port 8646 on target
+// - Authenticates based on presence of whitelisted tokens (which are sha256 hashes)
 
-const httpProxy = require('http-proxy'),
-      url = require('url'),
-      fs = require('fs'),
-      tlsConfig = require('./config').tls
-      // targetHost = 'localhost',
-      targetHost = "127.0.0.1",
-      targetHttpPort = 8545,
-      targetWsPort = 8546,
-      proxyPort = 3000;
-
-const {keyPath, certPath} = tlsConfig
-
+// main function
 const run = () => {
-  const proxy = createProxy()
+  const proxy = httpProxy.createProxyServer({})
   const server = handleWsRequests(createServer(proxy), proxy)
-  // const server = handleRequests(createServer())
-  server.listen(proxyPort);
-  console.log(`Listening on HTTPS port ${proxyPort}`);
+  server.listen(proxyPort)
+
+  console.log(`Listening on HTTPS port ${proxyPort}`)
   return server
 }
 
-// const createProxy = () => {
-//   return httpProxy.createServer({
-//     // target: `http://${targetHost}:${targetHttpPort}`,
-//     // ssl: {
-//     //   key: fs.readFileSync(keyPath, 'utf8'),
-//     //   cert: fs.readFileSync(certPath, 'utf8')
-//     // }
-//   })
-// }
-
-const createProxy = () => {
-  return httpProxy.createProxyServer({
-  
+const createServer = proxy => (
+  http.createServer((req, res) => {
+    hasValidToken(req)
+      ? proxy.web(req, res, { target: `http://${targetHost}:${targetHttpPort}` })
+      : respondWith401(res)
   })
-}
-
-const createServer = proxy =>
-  http.createServer((req, res) => proxy.web(req, res, {
-      target: `http://${targetHost}:${targetHttpPort}`,
-      ssl: {
-        key: fs.readFileSync(keyPath, 'utf8'),
-        cert: fs.readFileSync(certPath, 'utf8')
-      }
-    })
-  )
-
-// proxy -> sever
-
-const handleRequests = proxy => handleHttpRequests(proxy)
-
-const handleHttpRequests = proxy =>
-  https.createServer((req, res) => proxy.web(req, res))
-
+)
 const handleWsRequests = (server, proxy) => {
   server.on("upgrade", (req, socket, head) => {
-    proxy.ws(req, socket, head, { target: `ws://${targetHost}:${targetWsPort}` })
+    hasValidToken(req)
+    ? proxy.ws(req, socket, head, { target: `ws://${targetHost}:${targetWsPort}` })
+    : socket.end("HTTP/1.1 401 Unauthorized\r\n\r\n")
   })
   return server
 }
 
+const hasValidToken = (req)  =>
+  tokens[url.parse(req.url, true).query['token']]
 
-// const handleRequests = server => handleProxyRequests(server)
-
-// const handleProxyRequests = server => {
-//   // 'proxyReq' event called after TLS handshake, before passing request to server
-//   server.on('proxyReq', (proxyReq, req, res, options) => {
-//     authenticate(req, res)
-//     if (isUpgrade(req)) createWsProxy(req, res)
-//   })
-//   return server
-// }
-
-// const handleWsUpgrade = server => {
-//   server.on('upgrade', (req, socket, head) => {
-//     console.log("Got WSS upgrade connection");
-//     createWsProxy()
-//   })
-//   return server
-// }
-
-const createWsProxy = (req, res) => {
-  console.log("wsProxy created")
-  const proxy = httpProxy.createProxyServer({
-    ws: true,
-    target: `ws://${targetHost}:${targetWsPort}`,
-    // ssl: {
-    //   key: fs.readFileSync(keyPath, 'utf8'),
-    //   cert: fs.readFileSync(certPath, 'utf8')
-    // }
-  })
-  proxy.ws(req, req.socket, req.headers)
-  // res.writeHead(302, {'Content-Type': 'application/json' })
+const respondWith401 = (res) => {
+  res.writeHead(401, {'Content-Type': 'application/json' })
+  res.write(JSON.stringify({ error: "access denied" }))
   res.end()
 }
-
-
-// const createWsProxyDumb = () =>
-//   httpProxy.createServer({
-//     ws: true,
-//     target: `ws://${targetHost}:${targetWsPort}`,
-//     ssl: {
-//       key: fs.readFileSync(keyPath, 'utf8'),
-//       cert: fs.readFileSync(certPath, 'utf8')
-//     }
-//   }).listen(targetWsPort)
-
-const isUpgrade = req =>
-  req.headers.connection === "Upgrade" &&
-  req.headers.upgrade === "websocket"
-
-const authenticate = (req, res)  => {
-  if (!tokens[parseToken(req)]) {
-    res.writeHead(401, {'Content-Type': 'application/json' })
-    res.write(JSON.stringify({ error: "access denied" }))
-    res.end()
-  }
-}
-
-const parseToken = (req) =>
-  url.parse(req.url, true).query['token']
-
 
 module.exports = {
   run,
--- test/proxy.spec.js
+++ test/proxy.spec.js
@@ -1,15 +1,14 @@
-const fs = require('fs')
 const http = require("http")
 const request = require('supertest')
 const {expect} = require("chai")
 const {run, targetHttpPort, targetHost, targetWsPort, proxyPort } = require("../src/proxy")
 const {tokens} = require("../src/config")
-const {client: WsClient, server: WsServer} = require('websocket')
 const WebSocket = require("ws")
 
 describe("secure proxy", () => {
+
   const token = Object.keys(tokens)[0]
-  const ca = fs.readFileSync('certs/ca/my-root-ca.crt.pem');
+
   const runTarget = () => http
     .createServer((req, res)  => {
       res.writeHead(200, { 'Content-Type': 'application/json' })
@@ -20,141 +19,71 @@ describe("secure proxy", () => {
 
   let proxy, target
 
-  describe("with TLS", () => {
-  
-    before(() => {
-      proxy = run()
-      target = runTarget()
+  before(() => {
+    proxy = run()
+    target = runTarget()
+  })
+
+  after(() => {
+    proxy.close()
+    target.close()
+  })
+
+  describe("for an HTTP request", () => {
+
+    const agent = request(`http://${targetHost}:${proxyPort}`)
+
+    it("proxies a request with a valid token to target server", async () => {
+      const response = await agent.post(`/?token=${token}`).send({bar: 'baz'}).expect(200)
+      expect(response.body).to.eql({ foo: "bar" })
     })
-  
-    after(() => {
-      proxy.close()
-      target.close()
+
+    it("rejects an request with an invalid token", async () => {
+      const res = await agent.post("/?token=malicious").send({steal: 'passwords!'}).expect(401)
+      expect(res.body).to.eql({ error: "access denied" })
     })
-    
-    describe("for an HTTP request", () => {
-  
-      const agent = request(`https://${targetHost}:${proxyPort}`)
-  
-      it("proxies a request with a valid token to target server", async () => {
-        const response = await agent
-          .post(`/?token=${token}`)
-          .ca(ca)
-          .send({bar: 'baz'})
-          .expect(200)
-    
-        expect(response.body).to.eql({ foo: "bar" })
-      })
-    
-      it("rejects an request with an invalid token", async () => {
-        const res = await agent
-          .post("/?token=malicious")
-          .ca(ca)
-          .send({steal: 'passwords!'})
-          .expect(401)
-      
-        expect(res.body).to.eql({ error: "access denied" })
-      })
-    
-      it("rejects a request with a missing token", async () => {
-        const res = await agent
-          .post("/?foo=bar")
-          .ca(ca)
-          .expect(401)
-      
-        expect(res.body).to.eql({ error: "access denied" })
-      })
-    })
-    
-    describe("for a WS connection", () => {
-      
-      it("upgrades to a WS connection", async () => {
-        const resp = await request(`https://${targetHost}:${proxyPort}`)
-          .post(`/?token=${token}`)
-          .ca(ca)
-          .set("Content-type", "application/json")
-          .set("Connection",  "upgrade")
-          .set("Upgrade", "websocket")
-          .expect(200)
 
-        expect(resp.body).to.eql({})
-      })
-
-
-      const options = {
-        tlsOptions: {
-          ca,
-          checkServerIdentity: (host, cert) => {
-            if (host != cert.subject.CN) return "checkServerIdentity: mismatched:" + host;
-          }
-        }
-      }
-
-      it("proxies a request with a valid token", done => {
-        // const client = new WsClient(options)
-        // const server =
-        // client.on('connect', done)
-        // client.connect(`ws://${targetHost}:${proxyPort}/?token=${token}`)
-        const server = new WebSocket.Server({port: targetWsPort })
-        server.on("connection", () => {
-          console.log("connected!")
-        })
-        server.on("message", data => {
-          console.log("message: ", data)
-          done()
-        })
-        const client = new WebSocket(`ws://${targetHost}:${proxyPort}/?token=${token}`)
-        client.on("open", () => client.send("FOOOBAR!!!!"))
-      })
+    it("rejects a request with a missing token", async () => {
+      const res = await agent.post("/?foo=bar").expect(401)
+      expect(res.body).to.eql({ error: "access denied" })
     })
   })
-  
-  
-  describe("without TLS", () => {
 
-    describe("for a websocket", () => {
-      let wsClient, wsServer
-      beforeEach(() => {
-        proxy = run()
-        wsServer = new WebSocket.Server({port: targetWsPort })
-        wsClient = new WebSocket(`ws://${targetHost}:${proxyPort}/?token=${token}`)
-      })
-      afterEach(() => {
-        wsServer.close()
-        wsClient.close()
-        proxy.close()
-      })
+  describe("for a WS connection", () => {
+
+    let wsClient, wsServer
+    before(() => wsServer = new WebSocket.Server({port: targetWsPort }))
+    after(() => wsServer.close())
+
+    describe("with a valid token", () => {
+
+      beforeEach(() => wsClient = new WebSocket(`ws://${targetHost}:${proxyPort}/?token=${token}`))
+      afterEach(() => wsClient.close())
 
       it("creates a proxied websocket connection", done => {
-        wsServer.on("connection", ws => ws.on("message", msg => { expect(msg).to.eql("foo"); done(); }))
+        wsServer.on("connection", ws => ws.on("message", msg => {
+          expect(msg).to.eql("foo")
+          done()
+        }))
         wsClient.on("open", () => wsClient.send("foo"))
       })
     })
-    
-    describe("for an htpp request", () => {
-      // TODO: it should close the connection!
-      const agent = request(`http://${targetHost}:${proxyPort}`)
 
-      beforeEach(() => {
-        proxy = run()
-        target = runTarget()
+    describe("with an invalid token", () => {
+
+      beforeEach(() => wsClient = new WebSocket(`ws://${targetHost}:${proxyPort}/?token=invalid`))
+
+      it("rejects a connection ;attempt", async () => {
+        wsClient.on("error", err => expect(err.message).to.eql("Unexpected server response: 401"))
       })
-      afterEach(() => {
-        proxy.close()
-        target.close()
-      })
-      
-      it("proxies a request to target server", async () => {
-        // TODO: it should close the connection!
-        const response = await agent
-          .post(`/?token=${token}`)
-          .ca(ca)
-          .send({bar: 'baz'})
-          .catch(e => e)
-    
-        expect(response.body).to.eql({ foo: "bar" })
-        // expect(err.code).to.eql("ECONNRESET")
-        // expect(err.message).to.eql("socket hang up")
+    })
+
+    describe("without any token", () => {
+
+      beforeEach(() => wsClient = new WebSocket(`ws://${targetHost}:${proxyPort}`))
+
+      it("rejects a connection attempt", async () => {
+        wsClient.on("error", err => expect(err.message).to.eql("Unexpected server response: 401"))
       })
     })
   })
