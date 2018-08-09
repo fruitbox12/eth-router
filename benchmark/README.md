a6affa48270e1c15e1c692a66ba6348cd3d61570
Kyle Dunn <kdunn926@gmail.com>
2018-08-09 16:27:32 -0600

    Adding benchmarking WIP

---
+++ benchmark/README.md
@@ -0,0 +1,52 @@
+# Gatling benchmarking for eth-router
+
+Some simple benchmarking.
+
+### How to run:
+
+0. Download and unpack [Gatling](https://gatling.io)
+1. Simplify later commands and reveal the munitions
+
+```bash
+  $ export GATLING_HOME=where/ever/you/put/it
+```
+
+2. Get situated
+
+```bash
+  $ cd ${GATLING_HOME}
+```
+
+3. Make a text file called `token.txt` containing a valid authentication token
+
+```bash
+  $ cat > token.txt
+mysupersecuretokeninplaintext
+^D
+```
+
+4. Make a directory with the simluations package name (e.g. `qs`) in 
+
+```bash
+  $ mkdir ${GATLING_HOME}/user-files/simulations/qs
+```
+
+5. Copy [this](SecureRPC.scala) file (`SecureRPC.scala`) into directory above
+
+6. Set up the environment 
+
+```bash
+  $ export JAVA_OPTS="-DSRPC_TOKEN=`cat ./token.txt` -DSRPC_DOMAIN=stg.rpc.mysweetdomain.com"
+```
+
+7. Let 'er rip
+
+```bash
+  $ ./bin/gatling.sh
+```
+
+### Debugging request/responses
+
+Change `WARN` to `DEBUG` in `${GATLING_HOME}/conf/logback.xml`
+
+Gatling docs [reference](https://gatling.io/docs/2.3/general/debugging/)
---
+++ benchmark/SecureRPC.scala
@@ -0,0 +1,115 @@
+package qs
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+/* 
+ * How to run:
+ *
+ * 0. Download and unpack Gatling, 
+ * 1. export GATLING_HOME=where/ever/you/put/it
+ * 2. cd ${GATLING_HOME}
+ * 3. Make a text file called `token.txt` containing a valid authentication token
+ * 4. Make a directory with this file's package name (e.g. `qs`) in ${GATLING_HOME}/user-files/simulations
+ * 5. Copy this file into that directory
+ * 6. export JAVA_OPTS="-DSRPC_TOKEN=`cat ./token.txt` -DSRPC_DOMAIN=stg.rpc.mysweetdomain.com"
+ * 7. ./bin/gatling.sh
+ *
+ */
+class SecureRPC extends Simulation {
+
+  /* TODO - our URL-based routing complicates this a bit
+  val mainnetHttpsUrl = System.getProperty("SRPC_HTTPS_MAIN")
+  val testnetHttpsUrl = System.getProperty("SRPC_HTTPS_TEST")
+  val mainnetWssUrl = System.getProperty("SRPC_WSS_MAIN")
+  val testnetWssUrl = System.getProperty("SRPC_WSS_TEST")
+  */
+
+  val rpcDomain = System.getProperty("SRPC_DOMAIN")
+  val rpcToken = System.getProperty("SRPC_TOKEN")
+  val testPayload = """{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":74}"""
+
+  val httpConf = http
+    .baseURL("https://" + rpcDomain)
+    .acceptHeader("application/json")
+    .wsBaseURL("wss://" + rpcDomain)
+
+  val httpsTestnetScenario = scenario("Secure HTTP - testnet")
+    .exec(session => session.set("key", rpcToken))
+    .exec(http("Heartbeat testnet")
+        .get("/")
+        .header("Content-Type", "application/json")
+        .queryParam("token", "${key}")
+        .check(status.is(200), bodyString.is("")))
+    .pause(1)
+    .repeat(2, "i") {
+      exec(http("Poke testnet JSON-RPC")
+        .post("/")
+        .header("Content-Type", "application/json")
+        .queryParam("token", "${key}")
+        .body(StringBody(testPayload)).asJSON
+        .check(status.is(200), jsonPath("$..startingBlock").ofType[String]))
+        // "startingBlock":"0x0"
+        .pause(1)
+    }
+
+  val httpsMainnetScenario = scenario("Secure HTTP - mainnet")
+    .exec(session => session.set("key", rpcToken))
+    .exec(http("Heartbeat mainnet")
+        .get("/mainnet")
+        .header("Content-Type", "application/json")
+        .queryParam("token", "${key}")
+        .check(status.is(200), bodyString.is("")))
+    .pause(1)
+    .repeat(2, "i") {
+      exec(http("Poke mainnet JSON-RPC")
+        .post("/mainnet")
+        .header("Content-Type", "application/json")
+        .queryParam("token", "${key}")
+        .body(StringBody(testPayload)).asJSON
+        .check(status.is(200), jsonPath("$..startingBlock").ofType[String]))
+        // "startingBlock":"0x0"
+        .pause(1)
+    }
+
+  val wssTestnetScenario = scenario("Secure WebSocket - testnet")
+    .exec(session => session.set("key", rpcToken))
+    .exec(http("testnet WSS home")
+      .get("/?token=${key}")
+      .header("Content-Type", "application/json"))
+    .exec(ws("Connect WSS testnet").open("/?token=${key}"))
+    .pause(1)
+    .repeat(2, "i") {
+      exec(ws("Poke testnet WSS")
+        .sendText(testPayload)
+        .check(wsAwait.within(3).until(1).jsonPath("$..startingBlock").ofType[String]))
+        // "startingBlock":"0x0"
+        .pause(1)
+    }
+    .exec(ws("Close WSS testnet").close)
+
+  val wssMainnetScenario = scenario("Secure WebSocket - mainnet")
+    .exec(session => session.set("key", rpcToken))
+    .exec(http("mainnet WSS home")
+      .get("/mainnet?token=${key}")
+      .header("Content-Type", "application/json"))
+    .exec(ws("Connect WSS mainnet").open("/mainnet?token=${key}"))
+    .pause(1)
+    .repeat(2, "i") {
+      exec(ws("Poke mainnet WSS")
+        .sendText(testPayload)
+        .check(wsAwait.within(3).until(1).jsonPath("$..startingBlock").ofType[String]))
+        // "startingBlock":"0x0"
+        .pause(1)
+    }
+    .exec(ws("Close WSS mainnet").close)
+
+  setUp(
+    httpsTestnetScenario.inject(atOnceUsers(1)),
+    httpsMainnetScenario.inject(atOnceUsers(1)),
+    wssTestnetScenario.inject(atOnceUsers(1)),
+    wssMainnetScenario.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
+
