814bddd3ef2b25fb89fe5fa421d3f1803c9bef3d
Lee Azzarello <lee@rockingtiger.com>
2018-08-17 13:08:36 -0700

    typo'd

--- deploy/terraform/variables-example.sh
+++ deploy/terraform/variables-example.sh
@@ -1,14 +1,9 @@
-set -e
-set -x
-printf "This script will load an environment which you can use to bootstrap a new eth-router cluster\n"
-
 me=`whoami`
-
-export TF_shared_credentials_file="/Users/${me}/.aws/credentials" 
-export TF_shared_credentials_profile="example-profile" 
-export TF_rpc_proxy_key_name="example-key" 
-export TF_blockchain_key_name="example-key" 
-export TF_region="region"
-export TF_dns_name="rpc.example.com"
+export TF_VAR_shared_credentials_file="/Users/${me}/.aws/credentials" 
+export TF_VAR_shared_credentials_profile="example-profile" 
+export TF_VAR_rpc_proxy_key_name="example-key" 
+export TF_VAR_blockchain_key_name="example-key" 
+export TF_VAR_region="region"
+export TF_VAR_dns_name="rpc.example.com"
 # probably okay to keep these in here
-export TF_ami_map='{ us-west-2 = "ami-79873901", us-east-1 = "ami-b374d5a5", us-east-2 = "ami-82f4dae7" }'
+export TF_VAR_ami_map='{ us-west-2 = "ami-79873901", us-east-1 = "ami-b374d5a5", us-east-2 = "ami-82f4dae7" }'
