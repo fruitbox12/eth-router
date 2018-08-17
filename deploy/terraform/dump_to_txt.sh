5bf9b7fcc6a6d59f259e8227d09c1a3c74089dd5
Lee Azzarello <lee@rockingtiger.com>
2018-08-17 13:25:15 -0700

    notes for SECRETS_PATH

--- deploy/terraform/dump_to_txt.sh
+++ deploy/terraform/dump_to_txt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # TODO refactor to a relative or dependable path
-(cd ~/Desktop/quantstamp/secrets && sops -d ansible-playbooks.yml | python -c 'import sys, yaml; print(yaml.load(sys.stdin)["rpc_proxy_vault"])')
+printf "Need to set \$SECRETS_PATH\n"
+(cd $SECRETS_PATH && sops -d ansible-playbooks.yml | python -c 'import sys, yaml; print(yaml.load(sys.stdin)["rpc_proxy_vault"])')
