678146c44f2bad468c34eeb83592f598457f67dd
Lee Azzarello <lee@rockingtiger.com>
2018-08-30 11:47:49 -0700

    expose graphs via nginx

--- deploy/terraform/modules/blockchain/eth_node.tf
+++ deploy/terraform/modules/blockchain/eth_node.tf
@@ -80,6 +80,14 @@ resource "aws_security_group" "eth_node_sg" {
       "0.0.0.0/0"
     ]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
   egress {
     from_port   = 0
     to_port     = 0
